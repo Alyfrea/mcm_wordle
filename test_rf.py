2695c90ccf4f2a9aa558aba01047a6db281003f2
Alyfrea <90093737+Alyfrea@users.noreply.github.com>
2023-02-18 01:14:33 +0000

    dfsdf

--- test_rf.py
+++ test_rf.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
- 
+
 df = pd.read_csv('question2.csv')
 y = df['ex'].values
 x = df.drop(columns=['ex', 'dx'])
